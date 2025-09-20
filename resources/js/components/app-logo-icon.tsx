393f85bec6b989a72bcffe1bf29d065eb9026ee2
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-09-20 15:12:49 +0100

    update logo image

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,6 +2,6 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <img  src="./czm_logo.png" alt="CZM"  />
+        <img  src="./czm_Logo.png" alt="CZM"  />
     );
 }
